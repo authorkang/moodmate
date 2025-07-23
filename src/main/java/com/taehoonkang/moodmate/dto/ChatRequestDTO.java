7af27f0608acf1e36488ba65e4fdc05c7365cfc6
authorkang <114459988+authorkang@users.noreply.github.com>
2025-07-23 20:10:51 +0100

    Add files via upload

---
+++ src/main/java/com/taehoonkang/moodmate/dto/ChatRequestDTO.java
@@ -0,0 +1,10 @@
+package com.taehoonkang.moodmate.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatRequestDTO {
+    private String requestText;
+}
---
+++ src/test/java/com/taehoonkang/moodmate/MoodmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taehoonkang.moodmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
